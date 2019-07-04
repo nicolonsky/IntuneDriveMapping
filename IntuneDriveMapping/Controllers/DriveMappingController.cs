6b11ac8864c5a3bd334f7dc192375b7b8f3268fe
nicolonsky <nicola@nicolonsky.ch>
2019-07-04 15:39:55 +0200

    Initial

---
+++ IntuneDriveMapping/Controllers/DriveMappingController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Xml;
+using IntuneDriveMapping.Models;
+using System.Web;
+
+namespace IntuneDriveMapping.Controllers
+{
+    public class DriveMappingController : Controller
+    {
+        // GET: Users  
+        public ActionResult Index()
+        {
+            if (TempData["userData"] == null)
+            {
+                ViewBag.ShowList = false;
+                return View();
+            }
+            else
+            {
+                List<DriveMappingModel> lst = (List<DriveMappingModel>)TempData["userData"];
+                ViewBag.ShowList = true;
+                return View(lst);
+            }
+        }
+        [HttpPost]
+        public ActionResult Upload()
+        {
+            try
+            {
+                List<DriveMappingModel> userList = new List<DriveMappingModel>();
+                var file = Request.Form.Files[0];
+                if (file != null && file.Length > 0)
+                {
+                    XmlDocument xmldoc = new XmlDocument();
+                    xmldoc.Load(file.OpenReadStream());
+                    DriveMappingModel user;
+                    XmlNodeList usernodes = xmldoc.SelectNodes("gpo/User/ExtensionData/Extension/DriveMapSettings/Drive/Properties");
+                    foreach (XmlNode usr in usernodes)
+                    {
+                        user = new DriveMappingModel
+                        {
+                            Id = Convert.ToInt32(usr["id"].InnerText),
+                            Path = usr["path"].InnerText,
+                            DriveLetter = usr["letter"].InnerText,
+                            Label = usr["label"].InnerText
+                        };
+
+                        userList.Add(user);
+                    }
+                    TempData["userData"] = userList;
+                }
+                return RedirectToAction("DriveMapping");
+            }
+            catch (Exception ex)
+            {
+                var error = ex;
+                throw;
+            }
+        }
+    }
+}
+
+
+
+
---
+++ IntuneDriveMapping/Models/DriveMappingModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntuneDriveMapping.Models
+{
+    public class DriveMappingModel
+    {
+        public int Id { get; set; }
+        public string Path { get; set; }
+        public string DriveLetter { get; set; }
+        public string Label { get; set; }
+    }
+}
