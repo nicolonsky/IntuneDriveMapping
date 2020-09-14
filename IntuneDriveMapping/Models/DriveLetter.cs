76f7844319fb2b5f7e0f9a534a9f11905cc4ff9d
Nicola Suter <nicola@nicolonsky.ch>
2020-09-14 22:17:37 +0200

    dropdown for drive letter and UI improvements

---
+++ IntuneDriveMapping/Models/DriveLetter.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntuneDriveMapping.Models
+{
+    public class DriveLetter
+    {
+        public static IEnumerable<SelectListItem> GetDriveLetters()
+        {
+            List<SelectListItem> selectListItems = new List<SelectListItem>();
+            char[] chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+
+            foreach (Char letter in chars)
+            {
+                selectListItems.Add(new SelectListItem
+                {
+                    Text = new string(letter + ":"),
+                    Value = letter.ToString()
+                });
+            }
+
+            return selectListItems;
+        }
+    }
+}
